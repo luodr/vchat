9d5442e42143859f275f01e8a17eea2621657f39
ldr <874471992@qq.com>
2021-01-08 11:31:12 +0800

    romove old client

--- client/src/api/friend.js
+++
@@ -1,7 +0,0 @@
-import { post, get } from "./request";
-export const getFriends =  data => {
-  return get("friend/list", data);
-};
-export const deleteFriends =  data => {
-  return  post("friend/deleteFriend", data);
-};
